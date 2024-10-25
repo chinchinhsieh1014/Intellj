943a00dabf1d49dd4e0b586eb3d4e3a318dbe66c
YC,Hsieh <79919595+chinchinhsieh1014@users.noreply.github.com>
2024-10-25 16:15:32 +0100

    Add files via upload

---
+++ EnumExample.java
@@ -0,0 +1,31 @@
+package note;
+
+import java.util.Random;
+
+enum DayOfTheWeek {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+
+    public double getTime() {
+        return switch (this) {
+            case SUNDAY, SATURDAY -> 1.0;
+            default -> 0.2;
+        };
+    }
+}
+public class EnumExample {
+    public static DayOfTheWeek getRandomDay() {
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+        return allDays[randomInteger];
+    }
+
+    public static void main(String[] args) {
+        DayOfTheWeek day = DayOfTheWeek.MONDAY;
+        System.out.println(day);
+        System.out.println(day.name());
+        System.out.println(day.ordinal());
+        System.out.println(day.getTime());
+
+        System.out.println(getRandomDay());
+    }
+}
